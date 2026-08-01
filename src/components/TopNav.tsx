9ac81771e3a48a67dc6299a544d35db8838c4eda
huazhechen <37705123@qq.com>
2026-08-01 20:56:29 +0800

    Simplify editor navigation

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -8,7 +8,6 @@ export function TopNav() {
         <span>M3N</span>
       </NavLink>
       <nav aria-label="主导航">
-        <NavLink to="/editor">在线编辑</NavLink>
         <NavLink to="/scores">乐谱库</NavLink>
         <NavLink to="/docs">文档</NavLink>
       </nav>
