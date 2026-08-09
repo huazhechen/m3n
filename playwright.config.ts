3ea8d0d923a38338a5c957cb12f8f5e2deb606d2
huazhechen <37705123@qq.com>
2026-08-09 22:45:42 +0800

    test: use system chromium when available

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,8 @@
 import { defineConfig } from '@playwright/test'
+import { existsSync } from 'node:fs'
+
+const systemChromium = ['/usr/bin/chromium', '/usr/sbin/chromium', '/usr/bin/chromium-browser']
+  .find((path) => existsSync(path))
 
 export default defineConfig({
   testDir: './tests/e2e',
@@ -8,7 +12,8 @@ export default defineConfig({
   use: {
     baseURL: 'http://127.0.0.1:4173',
     browserName: 'chromium',
-    channel: 'msedge',
+    channel: systemChromium ? undefined : 'msedge',
+    launchOptions: systemChromium ? { executablePath: systemChromium } : undefined,
     headless: true,
   },
   webServer: {
